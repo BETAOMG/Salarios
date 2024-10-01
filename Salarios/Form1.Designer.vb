8d7d8b85ea73ed4f5205ef093bbf4111b185f6c3
BETAOMG <luisdavidbch@gmail.com>
2024-10-01 17:40:12 -0500

    Agregar nombres de variables

--- Salarios/Form1.Designer.vb
+++ Salarios/Form1.Designer.vb
@@ -24,16 +24,14 @@ Partial Class Form1
     Private Sub InitializeComponent()
         Label1 = New Label()
         TextBox1 = New TextBox()
+        TextBox2 = New TextBox()
         Label2 = New Label()
         Label3 = New Label()
         Label4 = New Label()
-        Label5 = New Label()
-        Label6 = New Label()
-        Label7 = New Label()
-        Label8 = New Label()
-        Label9 = New Label()
-        Label10 = New Label()
-        Label11 = New Label()
+        TextBox3 = New TextBox()
+        TextBox4 = New TextBox()
+        TextBox5 = New TextBox()
+        Button1 = New Button()
         SuspendLayout()
         ' 
         ' Label1
@@ -41,122 +39,96 @@ Partial Class Form1
         Label1.AutoSize = True
         Label1.Location = New Point(40, 25)
         Label1.Name = "Label1"
-        Label1.Size = New Size(128, 15)
+        Label1.Size = New Size(131, 15)
         Label1.TabIndex = 0
-        Label1.Text = "Numero de empleados"
+        Label1.Text = "Numero de empleados:"
         ' 
         ' TextBox1
         ' 
-        TextBox1.Location = New Point(192, 25)
+        TextBox1.Location = New Point(177, 22)
         TextBox1.Name = "TextBox1"
         TextBox1.Size = New Size(54, 23)
         TextBox1.TabIndex = 1
         ' 
+        ' TextBox2
+        ' 
+        TextBox2.Location = New Point(41, 65)
+        TextBox2.Multiline = True
+        TextBox2.Name = "TextBox2"
+        TextBox2.Size = New Size(205, 202)
+        TextBox2.TabIndex = 2
+        ' 
         ' Label2
         ' 
         Label2.AutoSize = True
-        Label2.Location = New Point(45, 108)
+        Label2.Location = New Point(41, 288)
         Label2.Name = "Label2"
-        Label2.Size = New Size(126, 15)
-        Label2.TabIndex = 2
-        Label2.Text = "Nombre del empleado"
+        Label2.Size = New Size(159, 15)
+        Label2.TabIndex = 3
+        Label2.Text = "Total de retencion recaudada"
         ' 
         ' Label3
         ' 
         Label3.AutoSize = True
-        Label3.Location = New Point(122, 143)
+        Label3.Location = New Point(41, 315)
         Label3.Name = "Label3"
-        Label3.Size = New Size(46, 15)
-        Label3.TabIndex = 3
-        Label3.Text = "Código"
+        Label3.Size = New Size(153, 15)
+        Label3.TabIndex = 4
+        Label3.Text = "Total de subsidios aplicados"
         ' 
         ' Label4
         ' 
+        Label4.Anchor = AnchorStyles.None
         Label4.AutoSize = True
-        Label4.Location = New Point(45, 173)
+        Label4.Location = New Point(37, 343)
         Label4.Name = "Label4"
-        Label4.Size = New Size(74, 15)
-        Label4.TabIndex = 4
-        Label4.Text = "Salario bruto"
+        Label4.Size = New Size(349, 15)
+        Label4.TabIndex = 5
+        Label4.Text = "Salario promedio de los empleados que ganan mas de 3 millones"
         ' 
-        ' Label5
+        ' TextBox3
         ' 
-        Label5.AutoSize = True
-        Label5.Location = New Point(45, 210)
-        Label5.Name = "Label5"
-        Label5.Size = New Size(69, 15)
-        Label5.TabIndex = 5
-        Label5.Text = "Salario neto"
+        TextBox3.Location = New Point(218, 288)
+        TextBox3.Name = "TextBox3"
+        TextBox3.Size = New Size(137, 23)
+        TextBox3.TabIndex = 6
         ' 
-        ' Label6
+        ' TextBox4
         ' 
-        Label6.AutoSize = True
-        Label6.Location = New Point(45, 242)
-        Label6.Name = "Label6"
-        Label6.Size = New Size(80, 15)
-        Label6.TabIndex = 6
-        Label6.Text = "Valor subsidio"
+        TextBox4.Location = New Point(218, 317)
+        TextBox4.Name = "TextBox4"
+        TextBox4.Size = New Size(137, 23)
+        TextBox4.TabIndex = 7
         ' 
-        ' Label7
+        ' TextBox5
         ' 
-        Label7.AutoSize = True
-        Label7.Location = New Point(324, 108)
-        Label7.Name = "Label7"
-        Label7.Size = New Size(102, 15)
-        Label7.TabIndex = 7
-        Label7.Text = "Valor de retencion"
+        TextBox5.Location = New Point(405, 340)
+        TextBox5.Name = "TextBox5"
+        TextBox5.Size = New Size(133, 23)
+        TextBox5.TabIndex = 8
         ' 
-        ' Label8
+        ' Button1
         ' 
-        Label8.AutoSize = True
-        Label8.Location = New Point(324, 143)
-        Label8.Name = "Label8"
-        Label8.Size = New Size(74, 15)
-        Label8.TabIndex = 8
-        Label8.Text = "Total a pagar"
-        ' 
-        ' Label9
-        ' 
-        Label9.AutoSize = True
-        Label9.Location = New Point(324, 186)
-        Label9.Name = "Label9"
-        Label9.Size = New Size(159, 15)
-        Label9.TabIndex = 9
-        Label9.Text = "Total de retencion recaudada"
-        ' 
-        ' Label10
-        ' 
-        Label10.AutoSize = True
-        Label10.Location = New Point(324, 214)
-        Label10.Name = "Label10"
-        Label10.Size = New Size(153, 15)
-        Label10.TabIndex = 10
-        Label10.Text = "Total de subsidios aplicados"
-        ' 
-        ' Label11
-        ' 
-        Label11.AutoSize = True
-        Label11.Location = New Point(324, 242)
-        Label11.Name = "Label11"
-        Label11.Size = New Size(414, 15)
-        Label11.TabIndex = 11
-        Label11.Text = "Salario promedo de los empledaos que ganan mas de 3000000 de salario neto"
+        Button1.Location = New Point(37, 376)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(75, 23)
+        Button1.TabIndex = 9
+        Button1.Text = "Calcular"
+        Button1.UseVisualStyleBackColor = True
         ' 
         ' Form1
         ' 
-        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleDimensions = New SizeF(7.0F, 15.0F)
         AutoScaleMode = AutoScaleMode.Font
-        ClientSize = New Size(800, 450)
-        Controls.Add(Label11)
-        Controls.Add(Label10)
-        Controls.Add(Label9)
-        Controls.Add(Label8)
-        Controls.Add(Label7)
-        Controls.Add(Label6)
-        Controls.Add(Label5)
+        ClientSize = New Size(942, 483)
+        Controls.Add(Button1)
+        Controls.Add(TextBox5)
+        Controls.Add(TextBox4)
+        Controls.Add(TextBox3)
         Controls.Add(Label4)
         Controls.Add(Label3)
         Controls.Add(Label2)
+        Controls.Add(TextBox2)
         Controls.Add(TextBox1)
         Controls.Add(Label1)
         Name = "Form1"
@@ -167,14 +139,12 @@ Partial Class Form1
 
     Friend WithEvents Label1 As Label
     Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox2 As TextBox
     Friend WithEvents Label2 As Label
     Friend WithEvents Label3 As Label
     Friend WithEvents Label4 As Label
-    Friend WithEvents Label5 As Label
-    Friend WithEvents Label6 As Label
-    Friend WithEvents Label7 As Label
-    Friend WithEvents Label8 As Label
-    Friend WithEvents Label9 As Label
-    Friend WithEvents Label10 As Label
-    Friend WithEvents Label11 As Label
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents TextBox4 As TextBox
+    Friend WithEvents TextBox5 As TextBox
+    Friend WithEvents Button1 As Button
 End Class
--- Salarios/Form1.vb
+++ Salarios/Form1.vb
@@ -1,3 +1,19 @@
 ﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        'NumeroEmpleados Numero de empleados
+        'SalarioBruto Salario Bruto
+        'Nombre Nombre del empleado
+        'Codigo codigo del empleado
+        'SalarioNeto Salario neto
+        'Subsidio Valor del subsidio
+        'Retencion Valor de la retencion
+        'TotalPagar Total a pagar
+        'SumaRetencion Suma de retenciones
+        'SumaSubsidios Suma de subsidios
+        'SumaSalarios Suma de salarios mayores a 3 millones
+        'CuentaSalarios Contador de salarios
 
+
+
+    End Sub
 End Class
